73d98c7c3ff2bf2cc3e2e2d8e78a4e16cb22ac53
Jafet <jafet@XLP.local>
2020-06-29 08:41:53 -0500

    docker y diagrama ER

---
+++ inserts.sql
@@ -0,0 +1,46 @@
+insert into seller values (80,'mazda');
+insert into seller values (81,'bmw');
+
+insert into channel values (1,'gmail', 'smtp.gmail.com', '12jafettest@gmail.com', 'contrasea', 587, '12jafettest@gmail.com');
+insert into channel values (2,'elastic email', 'smtp.elasticemail.com', 'm-jafet@hotmail.com', '1FE27EADA983214CEF6143795B0467A49837', 2525, 'm-jafet@hotmail.com');
+
+insert into process values (20, 'servicio',1,80);
+insert into process values (21, 'pintura',2,80);
+insert into process values (22, 'refacciones',1,80);
+insert into process values (23, 'cotizacion',2,80);
+insert into process values (24, 'prueba',1,80);
+insert into process values (25, 'contacto',2,80);
+
+insert into process values (26, 'servicio',2,81);
+insert into process values (27, 'pintura',1,81);
+insert into process values (28, 'refacciones',2,81);
+insert into process values (29, 'cotizacion',1,81);
+insert into process values (30, 'prueba',2,81);
+insert into process values (31, 'contacto',1,81);
+
+insert into addresse values (101,'m-jafet@hotmail.com','Juan101','Perez','agente servicio',20,80);
+insert into addresse values (102,'m-jafet@hotmail.com','Juan102','Perez','agente servicio2',20,80);
+insert into addresse values (103,'m-jafet@hotmail.com','Juan103','Perez','agente pintura',21,80);
+insert into addresse values (104,'m-jafet@hotmail.com','Juan104','Perez','agente pintura2',21,80);
+insert into addresse values (105,'m-jafet@hotmail.com','Juan105','Perez','agente refacciones',22,80);
+insert into addresse values (106,'m-jafet@hotmail.com','Juan106','Perez','agente refacciones2',22,80);
+insert into addresse values (107,'m-jafet@hotmail.com','Juan107','Perez','agente cotizacion',23,80);
+insert into addresse values (108,'m-jafet@hotmail.com','Juan108','Perez','agente cotizacion2',23,80);
+insert into addresse values (109,'m-jafet@hotmail.com','Juan109','Perez','agente prueba',24,80);
+insert into addresse values (110,'m-jafet@hotmail.com','Juan110','Perez','agente prueba2',24,80);
+insert into addresse values (111,'m-jafet@hotmail.com','Juan111','Perez','gerencia',25,80);
+insert into addresse values (112,'m-jafet@hotmail.com','Juan112','Perez','gerencia2',25,80);
+
+insert into addresse values (113,'m-jafet@hotmail.com','Juan113','Perez','agente servicio',26,81);
+insert into addresse values (114,'m-jafet@hotmail.com','Juan114','Perez','agente servicio2',26,81);
+insert into addresse values (115,'m-jafet@hotmail.com','Juan115','Perez','agente pintura',27,81);
+insert into addresse values (116,'m-jafet@hotmail.com','Juan116','Perez','agente pintura2',27,81);
+insert into addresse values (117,'m-jafet@hotmail.com','Juan117','Perez','agente refacciones',28,81);
+insert into addresse values (118,'m-jafet@hotmail.com','Juan118','Perez','agente refacciones2',28,81);
+insert into addresse values (119,'m-jafet@hotmail.com','Juan119','Perez','agente cotizacion',29,81);
+insert into addresse values (120,'m-jafet@hotmail.com','Juan120','Perez','agente cotizacion2',29,81);
+insert into addresse values (121,'m-jafet@hotmail.com','Juan121','Perez','agente prueba',30,81);
+insert into addresse values (122,'m-jafet@hotmail.com','Juan122','Perez','agente prueba2',30,81);
+insert into addresse values (123,'m-jafet@hotmail.com','Juan123','Perez','gerencia',31,81);
+insert into addresse values (124,'m-jafet@hotmail.com','Juan124','Perez','gerencia2',31,81);
+
